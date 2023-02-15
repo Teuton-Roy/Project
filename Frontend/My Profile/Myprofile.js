fc80a17627f591869e6a29933073ee2355369e75
Teuton Roy <popeyehk404@gmail.com>
2023-02-15 22:44:06 +0530

    add myprofile dashboard html, css code

---
+++ Frontend/My Profile/Myprofile.js
@@ -0,0 +1,24 @@
+// Get the canvas element and create a new Chart object
+var canvas = document.getElementById('improvement-chart');
+var chart = new Chart(canvas, {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [{
+      label: 'Improvement',
+      data: [5, 10, 15, 20, 25, 30],
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
