1f8a4d7de3a0643b3ef3ca011cbc1f209d698f05
Teuton Roy <popeyehk404@gmail.com>
2023-02-17 19:28:57 +0530

    change in chart code: now evey student can see how much they are improve in skills by every month

--- Frontend/My Profile/Myprofile.js
+++ Frontend/My Profile/Myprofile.js
@@ -3,25 +3,55 @@ var ctx = document.getElementById('myChart').getContext('2d');
 
 // Create the chart
 var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    // labels: ['Coding', 'Development', 'Design', 'Mock-Test', 'Project-Verification', 'CS-Subjects',],
-    datasets: [{
-      label: 'Improvement',
-      data: [12, 19, 3, 5, 2, 10, 8, 9.5, 15, 18, 20, 11],
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
+    type: 'line',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      datasets: [{
+        label: 'Coding',
+        data: [12, 19, 3, 5, 2, 10, 8, 9.5, 15, 18, 20, 11],
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+      }, {
+        label: 'Development',
+        data: [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26],
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1
+      }, {
+        label: 'Design',
+        data: [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32],
+        backgroundColor: 'rgba(255, 206, 86, 0.2)',
+        borderColor: 'rgba(255, 206, 86, 1)',
+        borderWidth: 1
+      }, {
+        label: 'Project-Verification',
+        data: [5, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28],
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+      }, {
+        label: 'Spoken English',
+        data: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
+        backgroundColor: 'rgba(153, 102, 255, 0.2)',
+        borderColor: 'rgba(153, 102, 255, 1)',
+        borderWidth: 1
+      }, {
+        label: 'Mock-Test',
+        data: [10, 11, 9, 8, 11, 12, 13, 12, 14, 15, 16, 17],
+        backgroundColor: 'rgba(255, 159, 64, 0.2)',
+        borderColor: 'rgba(255, 159, 64, 1)',
+        borderWidth: 1
       }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
     }
-  }
-});
+  });
+  
