bb03a9093b5749ec708896339d8b1a968e366dcc
Teuton Roy <popeyehk404@gmail.com>
2023-02-17 19:20:26 +0530

    with some minor change chart js code set on Myprofile.js

--- Frontend/My Profile/Myprofile.js
+++ Frontend/My Profile/Myprofile.js
@@ -1,24 +1,27 @@
-// // Get the canvas element and create a new Chart object
-// var canvas = document.getElementById('improvement-chart');
-// var chart = new Chart(canvas, {
-//   type: 'line',
-//   data: {
-//     labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-//     datasets: [{
-//       label: 'Improvement',
-//       data: [5, 10, 15, 20, 25, 30],
-//       backgroundColor: 'rgba(54, 162, 235, 0.2)',
-//       borderColor: 'rgba(54, 162, 235, 1)',
-//       borderWidth: 1
-//     }]
-//   },
-//   options: {
-//     scales: {
-//       yAxes: [{
-//         ticks: {
-//           beginAtZero: true
-//         }
-//       }]
-//     }
-//   }
-// });
+// Get the canvas element
+var ctx = document.getElementById('myChart').getContext('2d');
+
+// Create the chart
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    // labels: ['Coding', 'Development', 'Design', 'Mock-Test', 'Project-Verification', 'CS-Subjects',],
+    datasets: [{
+      label: 'Improvement',
+      data: [12, 19, 3, 5, 2, 10, 8, 9.5, 15, 18, 20, 11],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
