3b24a16cdd9076c2e61ea8ced4308b2208f38678
Teuton Roy <popeyehk404@gmail.com>
2023-03-17 22:31:35 +0530

    add script

---
+++ Frontend/Toggle Dark Mode/script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('.togglebtn');
+const moveBtn = document.querySelector('.bgMove');
+const fa = document.querySelector('.fa-solid');
+const faMoon = document.querySelector('.fa-moon');
+
+btn.addEventListener('click', function () {
+    moveBtn.classList.toggle('active');
+
+    
+})
